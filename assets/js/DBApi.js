99c48f3ce3b4e9a057122f3d982157de80460c05
SX <1509774777@qq.com>
2020-10-06 11:43:09 +0800

    修改过后的登录页面01

--- assets/js/DBApi.js
+++ assets/js/DBApi.js
@@ -1,6 +1,13 @@
 //  当调用$.get() $.post() $.ajax() 函数时，会优先调用ajaxProfilter() 函数  可以获取给Ajax提供的配置对象
-$.ajaxProfilter(function (options) {
-    //  获取请求的路径地址
-    options.url = 'http://ajax.frontend.itheima.net' + options.url;
-    console.log(options.url);
+$(function () {
+    $.ajaxPrefilter(function (options) {
+        //  获取请求的路径地址
+        //  将网址进行拼接
+        options.url = 'http://ajax.frontend.itheima.net' + options.url;
+        
+        // console.log(options.url); 
+    });
+ 
 })
+
+
--- assets/js/login.js
+++ assets/js/login.js
@@ -44,8 +44,9 @@ $(function () {
                 password: $('#form_reg [name = password').val()
             },
             success: function (res) {
+                console.log(res);
                 if (res.status != 0) {
-                    return layer.mes(res.message);
+                    return layer.msg(res.message);
                 }
                 console.log('注册成功');
                 //  模拟人的点击行为
@@ -56,27 +57,31 @@ $(function () {
 
     //  监听登录表单的提交事件
     $('#form_login').on('submit', function (e) {
-        //  
+        //  监听登录表单的默认行为
         e.preventDefault();
-        // 
+        //  设置ajax请求
         $.ajax({
-            method: 'GET',
+            method: 'POST',
             url: '/api/login',
             data: $(this).serialize(),
             success: function (res) {
                 if (res.status != 0) {
-                    return  layer.mes('登录失败');
-                } 
-                layer.mes('登录成功！');
+                    return layer.msg('登录失败');
+                }
+                layer.msg('登录成功！');
+                console.log(res);
 
                 //  获取权限
                 //  将登陆成功获取的token值保存到localStorage 中
                 localStorage.setItem('token', res.token);
                 console.log(res.token);
 
+                
                 //  跳转至后台页面
                 location.href = './index.html';
             }
-        })
+        });
+        
+        
     })
 })
